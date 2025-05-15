4f879fdd65df583db3046fcfca8b6d4be319e883
alvaroffm <alvaroffm@gmail.com>
2025-05-15 23:35:32 +0200

    Added toast message and loading spinner

--- backend/app/logic.py
+++ backend/app/logic.py
@@ -1,5 +1,21 @@
+import datetime
+import time
+import random
+
+
 def get_rds_message():
-    return "Mensaje personalizado para RDS desde el backend."
+    time.sleep(1)
+    n = random.randint(0, 1)
+    now = datetime.datetime.now().time()
+    return (
+        (
+            True,
+            f" Son las {now}. \n Has pulsado modulo HNS. Mensaje obtenido desde el backend.",
+        )
+        if n == 0
+        else (False, f"Error a las {now}")
+    )
+
 
 def get_rdr_message():
-    return "Mensaje personalizado para RDR desde el backend."
+    return f"Has pulsado HNR. Hoy es {datetime.datetime.now().date().strftime('%d-%m-%Y')}. \n Mensaje personalizado para RDR desde el backend."
